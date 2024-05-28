0c250b182aa00f573de50c5cc13f2f9a58a7239d
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-05-27 22:20:43 -0300

    feat(prisma): User table

---
+++ prisma/migrations/20240528002413_user/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVATED', 'DEACTIVATED');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER', 'STUDENT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'ACTIVATED',
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_name_idx" ON "User"("name");
